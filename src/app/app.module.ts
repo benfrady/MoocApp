b0c555877fc9ac023c20bf1b9425c0e4156f1bf5
rapportst <youssef.benfradj@supcom.tn>
2020-03-29 20:31:54 +0100

    MoocApplication

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // Default module to import for application running in the browser
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
